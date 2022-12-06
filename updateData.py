c8f16aff1c760cee3d979f70cba95f4b2a749efc
Mark <markinoe@gmail.com>
2022-12-06 00:58:24 -0700

    Updated to reflect latest version

--- updateData.py
+++ updateData.py
@@ -1,14 +1,10 @@
-import time
-import traceback
-from pbwrap import Pastebin
-import json
-from pastemyst import Client, Language, Paste, Pasty, ExpiresIn, EditType
 import os
-from replit import db
-#from replit import db
 from inspect import getsourcefile
 from os.path import abspath
-    #set active directory to app location
+import json
+import boto3
+from replit import db
+#set active directory to file location
 directory = abspath(getsourcefile(lambda:0))
 #check if system uses forward or backslashes for writing directories
 if(directory.rfind("/") != -1):
@@ -17,6 +13,13 @@ else:
     newDirectory = directory[:(directory.rfind("\\")+1)]
 os.chdir(newDirectory)
 
+s3 = boto3.resource(
+    service_name='s3',
+    region_name='us-east-2',
+    aws_access_key_id=os.environ['aws_access_key_id'],
+    aws_secret_access_key=os.environ['aws_secret_access_key']
+)
+ 
 def getToken(tokenName):
 
     f = open("tokens.json")
@@ -33,11 +36,26 @@ def pasteData(text,persistence = "ONE_HOUR"):
     res4 = client.create_paste(Paste(pasties=[Pasty('ISO', text)], expires_in=ExpiresIn.ONE_HOUR))
   return("https://paste.myst.rs/"+res4.id)
 
-def getData(key):
-    return(db[key])
+def updateData(key,data):
+    filename = "data/" + key + ".json"
+    if key in ["listofpostsA","listofpostsB","listofpostsC"]:
+        with open(filename, 'w') as f:
+            json.dump({key:data},f)
+        s3.Bucket('epsilonbucket').upload_file(Filename=filename,Key=key+".json")
 
-def updateData(key, value):
-    db[key] = value
+    else:
+        db[key] = data
+    return
+
+def getData(key):
+    filename = "data/" + key + ".json"
+    if key in ["listofpostsA","listofpostsB","listofpostsC"]:
+        s3.Bucket('epsilonbucket').download_file(Filename = filename,Key=key+".json")
+        f = open(filename)
+        data = json.load(f)
+        return data[key]
+    else:
+        return db[key]
     return
 
 def listData():
@@ -47,13 +65,5 @@ def listData():
     if(key.find("listofposts") == -1):
         format = format + key + ": " + str(db[key]) + "\n"
     else:
-      format = format + "{}: {} items.\n".format(key,len(db[key]))
+      format = format + "{}: {} items.\n".format(key,len(getData(key)))
   return format
-
-def setup_db():
-    f = open("db.json")
-    data = json.load(f)
-    for key in data.keys():
-        print(key)
-        db[key] = data[key]
-    return
